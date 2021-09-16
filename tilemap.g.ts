05afcff2307a492df2518b2cf2fb57af4b0c6ee6
Jernej Porenta <jernej.porenta@3fs.si>
2021-09-17 01:10:55 +0200

    Update tilemap.g.jres, tilemap.g.ts

--- tilemap.g.ts
+++ tilemap.g.ts
@@ -27,9 +27,10 @@ namespace myTiles {
     //% fixedInstance jres blockIdentity=images._tile
     export const tile12 = image.ofBuffer(hex``);
 
-    helpers.registerTilemapFactory(function(name: string) {
+    helpers._registerFactory("tilemap", function(name: string) {
         switch(helpers.stringTrim(name)) {
-            case "level": return tiles.createTilemap(hex`0a0008000202020201020202020202020101010101010202020201010101010102020202010101010101020202020101010101010202020201010101010102020202010101010101020202020202020202020202`, img`
+            case "level":
+            case "level":return tiles.createTilemap(hex`0a0008000202020201020202020202020101010101010202020201010101010102020202010101010101020202020101010101010202020201010101010102020202010101010101020202020202020202020202`, img`
 2 2 2 2 . 2 2 2 2 2 
 2 2 . . . . . . 2 2 
 2 2 . . . . . . 2 2 
@@ -38,8 +39,9 @@ namespace myTiles {
 2 2 . . . . . . 2 2 
 2 2 . . . . . . 2 2 
 2 2 2 2 2 2 2 2 2 2 
-`, [myTiles.transparency16,myTiles.tile1,myTiles.tile2], TileScale.Sixteen)
-            case "level_0": return tiles.createTilemap(hex`0a0008000707070400030202070707070501000001060707070b0101000001010c07070b010100000101070707070101000001010c07070b0101000001010c0707070800000000090707070707070a0a07070702`, img`
+`, [myTiles.transparency16,myTiles.tile1,myTiles.tile2], TileScale.Sixteen);
+            case "level_0":
+            case "level_0":return tiles.createTilemap(hex`0a0008000707070400030202070707070501000001060707070b0101000001010c07070b010100000101070707070101000001010c07070b0101000001010c0707070800000000090707070707070a0a07070702`, img`
 2 2 2 2 . 2 2 2 2 2 
 2 2 . . . . . . 2 2 
 2 2 . . . . . . 2 2 
@@ -48,7 +50,26 @@ namespace myTiles {
 2 2 . . . . . . 2 2 
 2 2 . . . . . . 2 2 
 2 2 2 2 2 2 2 2 2 2 
-`, [myTiles.transparency16,myTiles.tile1,myTiles.tile2,myTiles.tile3,myTiles.tile4,myTiles.tile5,myTiles.tile6,myTiles.tile7,myTiles.tile8,myTiles.tile9,myTiles.tile10,myTiles.tile11,myTiles.tile12], TileScale.Sixteen)
+`, [myTiles.transparency16,myTiles.tile1,myTiles.tile2,myTiles.tile3,myTiles.tile4,myTiles.tile5,myTiles.tile6,myTiles.tile7,myTiles.tile8,myTiles.tile9,myTiles.tile10,myTiles.tile11,myTiles.tile12], TileScale.Sixteen);
+        }
+        return null;
+    })
+
+    helpers._registerFactory("tile", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "transparency16":return transparency16;
+            case "tile1":return tile1;
+            case "tile2":return tile2;
+            case "tile3":return tile3;
+            case "tile4":return tile4;
+            case "tile5":return tile5;
+            case "tile6":return tile6;
+            case "tile7":return tile7;
+            case "tile8":return tile8;
+            case "tile9":return tile9;
+            case "tile10":return tile10;
+            case "tile11":return tile11;
+            case "tile12":return tile12;
         }
         return null;
     })
